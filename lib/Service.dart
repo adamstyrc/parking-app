fa9780b609833ae4747683c55a7f3cac99b3a9e7
Artur Gurgul <arturgurgul@Arturs-MacBook-Pro-2.local>
2018-04-17 09:43:18 +0200

    example http request

--- lib/Service.dart
+++ lib/Service.dart
@@ -1,9 +1,10 @@
 
 import 'Models/Month.dart';
 import 'package:json/json.dart';
-import 'package:http/http.dart';
+//import 'package:http/http.dart';
 import 'package:http_parser/http_parser.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 class Service {
   static final _singleton = new Service._internal();
@@ -15,7 +16,7 @@ class Service {
 
   Service._internal();
 
-  void update() {
+  void updateOld() {
     var yearString = "2018";
     var monthString = "03";
     var url = baseURL + "parking/$yearString-$monthString-01";
@@ -23,18 +24,35 @@ class Service {
     print(url);
     
     var httpClient = new HttpClient();
-    var uri = new Uri.https('api.github.com', '/users/1');
-    
+    //var uri = new Uri.https('api.github.com', '/users/1');
 
-    var t = httpClient.get("mobileoffice-api-poc.azurewebsites.net", 443, "/ms-parkApp-0.0.1-SNAPSHOT/$baseURL");
+    var request = httpClient.get("office.freeworld.cloud", 80, "/user");
+        
     
-    
-    t.then((value) =>
+    request.then((value) =>
      print(value) 
     );
     //var request = await httpClient.getUrl(uri);
     //var response = await request.close();
     //var responseBody = await response.transform(UTF8.decoder).join();
     //return responseBody;
+  }
+
+  void update() {
+    new http.Client()
+   .get(
+     "http://office.freeworld.cloud/user",
+     headers: {
+       'Content-type': 'application/json',
+       'email': 'artur.gurgul@vattenfall.com',
+       'Authorization': 'token'
+     },
+     //body: '{"distinct": "users","key": "account","query": {"active":true}}'
+     )
+   .whenComplete(() => print('completed'))
+   .then((http.Response r) => r.body)
+   .then((body) =>
+    print(body) 
+   );
   }
 }
