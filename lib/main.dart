6f38ffb25d908ad06018606a37fcad03e44cfb77
light <styrc.adam@gmail.com>
2018-07-09 12:47:48 +0200

    Rename and extract Dashboard p2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobileoffice/ui/AccountView.dart';
-import 'Service.dart';
-import 'package:flutter_calendar/flutter_calendar.dart';
-import 'Calendarro.dart';
-import 'package:mobileoffice/ui/DaysView.dart';
-import 'package:mobileoffice/ui/PlannerView.dart';
 import 'package:mobileoffice/ui/Splash.dart';
 
 
@@ -13,26 +7,7 @@ void main() => runApp(MaterialApp(
   theme: new ThemeData(
     primaryColor: Colors.orange,
     accentColor: Colors.cyan[600],
-
   ),
   home: new Splash(),
 ));
-
-
-//class DashboardView extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: new ThemeData(
-//        primaryColor: Colors.orange,
-//        accentColor: Colors.cyan[600],
-//
-//      ),
-//      home: new MyHomePage(title: 'Vattenfall Parking'),
-//    );
-//  }
-//}
-
 
