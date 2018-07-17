87e9900cb16b2869d9d4130e9da54265ca9a27c7
light <styrc.adam@gmail.com>
2018-07-17 13:56:25 +0200

    Log inly when debug

--- lib/Utils/Logger.dart
+++ lib/Utils/Logger.dart
@@ -2,13 +2,17 @@
 class Logger {
 
   static void log(String log) {
-    String logLeftToPrint = log;
+    assert(() {
+      String logLeftToPrint = log;
 
-    while (logLeftToPrint.length > 200) {
-      print(logLeftToPrint.substring(0, 200));
-      logLeftToPrint = logLeftToPrint.substring(200, logLeftToPrint.length);
-    }
+      while (logLeftToPrint.length > 200) {
+        print(logLeftToPrint.substring(0, 200));
+        logLeftToPrint = logLeftToPrint.substring(200, logLeftToPrint.length);
+      }
 
-    print(logLeftToPrint);
+      print(logLeftToPrint);
+
+      return true;
+    }());
   }
 }
